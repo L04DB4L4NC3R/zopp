4e6f1a20e94e497306c04c8a196d84ea09cbbd68
angadsharma1016 <angadsharma1016@gmail.com>
2019-03-19 23:01:48 +0530

    view chain

--- controller/tp.go
+++ controller/tp.go
@@ -4,7 +4,13 @@
 package controller
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"log"
 	"net/http"
+
+	"github.com/ethereum/go-ethereum/rlp"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
@@ -23,6 +29,32 @@ func (c client) tester() http.HandlerFunc {
 		w.Write([]byte("client api working"))
 	}
 }
+
+func (c client) state(a common.Address, cc *ethclient.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		ctx := context.Background()
+		blk, err := cc.BlockByNumber(ctx, nil)
+		if err != nil {
+			log.Println(err)
+			w.Write([]byte("Some error occurred\n\n"))
+			return
+		}
+
+		var y interface{}
+
+		red := bytes.NewReader(blk.Bloom().Bytes())
+		if err = rlp.Decode(red, &y); err != nil {
+			log.Println(err)
+			w.Write([]byte("Some error occurred\n\n"))
+			return
+		}
+		log.Println(y)
+		json.NewEncoder(w).Encode(blk.Body().Transactions)
+		return
+	}
+}
 func (c client) RegisterRoutes(a common.Address, cc *ethclient.Client) {
 	http.HandleFunc("/api/v1/client/test", c.tester())
+	http.HandleFunc("/api/v1/client/state", c.state(a, cc))
 }
